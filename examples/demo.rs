9e08bc4260ae45860a7c4fa25ac230a8959883e1
GolDNenex <30295660+GolDNenex@users.noreply.github.com>
2025-11-19 15:17:18 +0100

    Making Clippy happy

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,21 +5,21 @@ async fn main() -> Result<(), Error> {
     //
     let client = Client::new();
 
-    let dslams = vec![
+    let dslams = [
         DSLAM::from("mon75-1"),
         DSLAM::new("mon", 75, 2),
         DSLAM::new("mon", 75, 3),
         DSLAM::new("xyz", 75, 1),
     ];
 
-    let nras = vec![
+    let nras = [
         NRA::from("mon75"),
         NRA::new("mar", 75),
         NRA::new("men", 75),
         NRA::new("xyz", 75),
     ];
 
-    let departements = vec![
+    let departements = [
         Departement::new(75),
         Departement::new(76),
         Departement::new(77),
@@ -28,13 +28,9 @@ async fn main() -> Result<(), Error> {
 
     let requests: Vec<Request> = dslams
         .iter()
-        .map(|dslam| Request::from(dslam))
-        .chain(nras.iter().map(|nra| Request::from(nra)))
-        .chain(
-            departements
-                .iter()
-                .map(|departement| Request::from(departement)),
-        )
+        .map(Request::from)
+        .chain(nras.iter().map(Request::from))
+        .chain(departements.iter().map(Request::from))
         .collect();
 
     println!("\nChecking requests");
